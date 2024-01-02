0079fea48da4be5b4e62ce7b0328ed6d576056d2
SvetaSaletska <svitlanagalevich@gmail.com>
2024-01-03 01:31:22 +0200

    update

---
+++ src/components/Feedback/feedback.jsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  render() {
+ return (
+  <div>
+    <h1></h1>
+    <div>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+    <h2></h2>
+    <p></p>
+    <p></p>
+    <p></p>
+
+  </div>
+ )
+  }
+
+}
+
+
+
+
+
+export default Feedback
