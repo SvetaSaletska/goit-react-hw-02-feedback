c0fd130d251d4b2d97d17ff0b25d1c02f3442e0a
SvetaSaletska <svitlanagalevich@gmail.com>
2024-01-03 16:10:52 +0200

    Creating feedback component

--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -10,17 +10,18 @@ class Feedback extends Component {
 
   render() {
  return (
-  <div>
-    <h1></h1>
+
+  <div className = {css.feedback}>
+    <p className = {css.request}>Please leave feedback</p>
     <div>
-      <button></button>
-      <button></button>
-      <button></button>
+      <button className = {css.btn}>Good</button>
+      <button className = {css.btn}>Neutral</button>
+      <button className = {css.btn}>Bad</button>
     </div>
-    <h2></h2>
-    <p></p>
-    <p></p>
-    <p></p>
+    <p className = {css.statistic}>Statistic</p>
+    <span className = {css.result}>Good</span>
+    <span className = {css.result}>Neutral</span>
+    <span className = {css.result}>Bad</span>
 
   </div>
  )
