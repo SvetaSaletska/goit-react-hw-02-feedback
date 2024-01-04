908ad45eb93a1637f81b0b045955b625250fea1b
SvetaSaletska <svitlanagalevich@gmail.com>
2024-01-04 17:15:11 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
-import { Feedback } from './Feedback/Feedback';
+import Feedback from './Feedback/Feedback';
+// import css from './App.module.css'
 
+ class App extends Component {
 
-
-
-class App extends Component {
-
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+    state = {
+      good: 0,
+      neutral: 0,
+      bad: 0
   }
+
+  onClickOpiniaBtn = (option) => {
+     this.setState((prev)=>({
+     [option]: prev[option] =+1
+}))
+
+console.log(this.state)
+  };
+
 render() {
-  const { good, bad, neutral } = this.state;
-}
+
   return (
-   <div className= {css.container}> 
-   <Feedback/>
-   </div>
-  );
+    <div>
+      <Feedback />
+    </div>
+   );
+}
 };
 
 
--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -1,29 +1,47 @@
 import { Component } from 'react';
 import css from '../Feedback/Feedback.module.css'
 
- export class Feedback extends Component {
+const Feedback = ({ options, onLeaveFeedback }) => {
+  return (
+      <div className={css.buttonsContainer}>
+          {options.map(option => (
+              <button
+                  type="button"
+                  className={css.btn}
+                  name={option}
+                  key={option}
+                  onClick={() => onLeaveFeedback(option)}>
+                  {option}
+              </button>
+          ))
+          }
+      </div>
+  );
+};
+
+//  export class Feedback extends Component {
 
 
-  render() {
- return (
+//   render() {
+//  return (
 
-  <div className = {css.feedback}>
-    <p className = {css.request}>Please leave feedback</p>
-    <div>
-      <button className = {css.btn}>Good</button>
-      <button className = {css.btn}>Neutral</button>
-      <button className = {css.btn}>Bad</button>
-    </div>
-    <p className = {css.statistic}>Statistic</p>
-    <span className = {css.result}>Good:</span>
-    <span className = {css.result}>Neutral:</span>
-    <span className = {css.result}>Bad:</span>
+//   <div className = {css.feedback}>
+//     <p className = {css.request}>Please leave feedback</p>
+//     <div>
+//       <button className = {css.btn }>Good</button>
+//       <button className = {css.btn }>Neutral</button>
+//       <button className = {css.btn }>Bad</button>
+//     </div>
+//     <p className = {css.statistic}>Statistic</p>
+//     <span className = {css.result}>Good:</span>
+//     <span className = {css.result}>Neutral:</span>
+//     <span className = {css.result}>Bad:</span>
 
-  </div>
- )
-  }
+//   </div>
+//  )
+//   }
 
-}
+// }
 
 
 
